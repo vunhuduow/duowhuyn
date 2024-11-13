9e4817338fc5b16f2f9ecc96b21bac78f427e303
vunhuduow <duongfitness3005@gmail.com>
2024-11-13 19:58:36 +0700

    upload source code

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = '';
+const giftUrl = 'https://vunhuduow.github.io/heart/';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc em bé của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const giftImage = 'meiihuyn.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
